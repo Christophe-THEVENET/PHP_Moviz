e432c37fb6355c6187af95179250cfe6f73210d3
Christophe THEVENET <christophethevenet2.0@gmail.com>
2024-08-02 14:21:51 +0200

    fix : conflit with asynchrone request

--- Api/change-approuved-review.php
+++ Api/change-approuved-review.php
@@ -3,18 +3,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-if (!isset($data['review_id']) || !isset($data['review_approuved'])) {
-    http_response_code(400);
-    echo json_encode(['error' => 'Missing required fields']);
-    exit;
-}
-
 $reviewId = (int)$data['review_id'];
 $reviewApprouved = (int)$data['review_approuved'];
 
 try {
 
-    $pdo = new PDO("mysql:dbname=" . _DB_NAME_ . ";host=" . _DB_HOST_ . ";charset=utf8mb4", _DB_USER_, _DB_PASSWORD_);
+    $pdo = new PDO("mysql:dbname=" . _DB_NAME_ . ";host=db;charset=utf8mb4", _DB_USER_, _DB_PASSWORD_);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $stmt = $pdo->prepare('UPDATE review SET approuved = :approuved WHERE id = :id');
